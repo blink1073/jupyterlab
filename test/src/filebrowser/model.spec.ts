bd0f2b3adb3c409ff04766437865c18a394dc8ce
Steven Silvester <steven.silvester@ieee.org>
2016-07-05 21:30:11 -0500

    wip filebrowser model tests

---
+++ test/src/filebrowser/model.spec.ts
@@ -0,0 +1,101 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+import expect = require('expect.js');
+
+import {
+  FileBrowserModel
+} from '../../../lib/filebrowser/model';
+
+
+describe('filebrowser/model', () => {
+
+  describe('FileBrowserModel', () => {
+
+    describe('#constructor()', () => {
+
+      it('should construct a new file browser model', () => {
+
+      });
+
+    });
+
+    describe('#pathChanged', () => {
+
+    });
+
+    describe('#refreshed', () => {
+
+    });
+
+    describe('#fileChanged', () => {
+
+    });
+
+    describe('#path', () => {
+
+    });
+
+    describe('#items', () => {
+
+    });
+
+    describe('#isDisposed', () => {
+
+    });
+
+    describe('#sessionIds', () => {
+
+    });
+
+    describe('#kernelspecs', () => {
+
+    });
+
+    describe('#dispose()', () => {
+
+    });
+
+    describe('#cd()', () => {
+
+    });
+
+    describe('#refresh()', () => {
+
+    });
+
+    describe('#copy()', () => {
+
+    });
+
+    describe('#delete()', () => {
+
+    });
+
+    describe('#download()', () => {
+
+    });
+
+    describe('#newUntitled()', () => {
+
+    });
+
+    describe('#rename()', () => {
+
+    });
+
+    describe('#upload()', () => {
+
+    });
+
+    describe('#shutdown()', () => {
+
+    });
+
+    describe('#startSession()', () => {
+
+    });
+
+  });
+
+});
