c3e2f4265e44abd50e2935fec5764ca26c8f207e
Afshin Darian <git@darian.af>
2016-10-12 11:24:10 +0100

    Add initial instance tracker test.

--- test/src/index.ts
+++ test/src/index.ts
@@ -2,6 +2,7 @@
 // Distributed under the terms of the Modified BSD License.
 
 import './common/activitymonitor.spec';
+import './common/instancetracker.spec';
 import './common/observablelist.spec';
 import './common/vdom.spec';
 
